90664c9b1d231b7440e20fcd24112cb8fb37d6b0
George <ladariugeo@gmail.com>
2026-06-11 08:41:43 -0500

    Agrega PWA para iOS

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'g316-memoriza-v1';
+const ASSETS = [
+  '/Memoriza/',
+  '/Memoriza/index.html',
+  '/Memoriza/manifest.json',
+  '/Memoriza/icon-192.png',
+  '/Memoriza/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
